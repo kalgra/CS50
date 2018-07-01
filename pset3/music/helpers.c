ce479522bec8523272450bb2deb5ece36e13d056
Kristian Algra <squidwisper@gmail.com>
2018-07-01 15:55:36 +0000

    added logic to frequency calculate

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -1,7 +1,7 @@
 // Helper functions for music
 
 #include <cs50.h>
-
+#include <math.h>
 #include "helpers.h"
 
 
@@ -11,7 +11,7 @@ int denom;
 float fract;
 int duration(string fraction)
 {
-    // store index 0 and 2 to integer values 'num' and 'denom' repectively use atoi
+    // store index 0 and 2 to integer values 'num' and 'denom' repectively use atoi to convert char to int.
     numer = atoi(fraction[0]);
     denom = atoi(fraction[2]);
     // return (numer / denom) / .125
@@ -22,7 +22,76 @@ int duration(string fraction)
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-  //TODO
+    // note address will always be at 0
+    // octave address will string lenth -1
+    int octave = note[strlen(note) - 1];
+    octave -= 48; // Adjust Ascii to int value
+
+    // Determine Frequency of note in the given octive
+    float freq = 0.0;
+
+     // Calculate the natural frequency based on note, i.e. not sharp or flat. Octave starts with C.
+     // base frequency is calculated as note A at the 4th octave.
+    switch(note[0])
+    {
+        case 'C' :
+            freq = 440.0 / (pow(2.0, (9.0 / 12.0)));
+            break;
+        case 'D' :
+            freq = 440.0 / (pow(2.0, (7.0 / 12.0)));
+            break;
+        case 'E' :
+            freq = 440.0 / (pow(2.0, (5.0 / 12.0)));
+            break;
+        case 'F' :
+            freq = 440.0 / (pow(2.0, (4.0 / 12.0)));
+            break;
+        case 'G' :
+            freq = 440.0 / (pow(2.0, (2.0 / 12.0)));
+            break;
+        case 'A' :
+            freq = 440.0;
+            break;
+        case 'B' :
+            freq = 440.0 * (pow(2.0, (2.0 / 12.0)));
+            break;
+        default :
+            return 0;
+    }
+
+    // Shift by full octaves.
+    // If the octave is greater than 4 multiply by 2.
+    if(octave > 4)
+    {
+        for(int i = 0; i < octave - 4; i++)
+        {
+            freq *= 2.0;
+        }
+    }
+    // If the octave is less than dive by 2.
+    else if(octave < 4)
+    {
+        for(int i = 0; i < 4 - octave; i++)
+        {
+            freq /= 2.0;
+        }
+    }
+
+    // Adjust for flat or sharp notes.
+    if(note[1] == 'b')
+    {
+        freq /= (pow(2.0, (1.0 / 12.0)));
+    }
+    else if(note[1] == '#')
+    {
+        freq *= (pow(2.0, (1.0 / 12.0)));
+    }
+
+
+    // Round the requecny before returning
+    int roundFreq = round(freq);
+    return roundFreq;
+
 }
 
 // Determines whether a string represents a rest
